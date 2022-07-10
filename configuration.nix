61dd71bce181e6b2b84f587c43c0d40d79fb2b90
rivera-bl <rivera.pablo1090@gmail.com>
2022-07-09 21:51:04 -0400

    configuration: add grub menu for uefi

--- configuration.nix
+++ configuration.nix
@@ -1,1 +1,27 @@
-{}
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  boot.loader.grub = {
+    enable = true;
+    devices = [ "nodev" ];
+    efiInstallAsRemovable = true;
+    efiSupport = true;
+  };
+
+
+  system.stateVersion = "22.05"; # Did you read the comment?
+
+}
+
