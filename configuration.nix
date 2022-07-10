843acbc44013f99c9697a9ddc254c25a0d81bc52
rivera-bl <rivera.pablo1090@gmail.com>
2022-07-09 21:51:48 -0400

    configuration: networking wireless

--- configuration.nix
+++ configuration.nix
@@ -20,6 +20,24 @@
     efiSupport = true;
   };
 
+  networking = {
+    hostName = "navi";
+
+    interfaces = {
+      enp0s31f6.useDHCP = true;
+      wlp0s20f3.useDHCP = true;
+    };
+
+    wireless = {
+      enable = true;
+      interfaces = [ "wlp0s20f3" ];
+      networks = {
+        "JMRivera." = {
+          pskRaw = "20e0852468f9cf0a1e848676bce12598fdff9668e778b6575bf64154e4e2fdef";
+        };
+      };
+    };
+  }; 
 
   system.stateVersion = "22.05"; # Did you read the comment?
 
