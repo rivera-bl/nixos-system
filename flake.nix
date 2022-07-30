727fed2322826ccd2e5190e339e2f17fcf9ec6d0
rivera-bl <rivera.pablo1090@gmail.com>
2022-07-30 18:25:34 +0000

    flake: update nvim input to local dir

--- flake.nix
+++ flake.nix
@@ -1,7 +1,8 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-22.05";
-    nvim.url = "github:rivera-bl/dotfiles?dir=nvim";
+    /* nvim.url = "github:rivera-bl/dotfiles?dir=nvim"; */
+    nvim.url = "/home/nixos/nixos/home/nvim";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
